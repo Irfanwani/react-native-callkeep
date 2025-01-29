f11b28e4bacdb9ca0d25c7b74fc3941d53adbd09
Irfan Wani <irfanwani347@gmail.com>
2025-01-29 19:18:46 +0530

    remove comments
    Code cleaning, nothing special

--- android/src/main/java/io/wazo/callkeep/RNCallKeepModule.java
+++ android/src/main/java/io/wazo/callkeep/RNCallKeepModule.java
@@ -428,11 +428,6 @@ public class RNCallKeepModule extends ReactContextBaseJavaModule implements Life
         this.hasListeners = false;
     }
 
-    // @ReactMethod
-    // public void displayIncomingCall(String uuid, String number, String callerName) {
-    //     this.displayIncomingCall(uuid, number, callerName, false, null);
-    // }
-
     @ReactMethod
     public void displayIncomingCall(String uuid, String number, String callerName, boolean hasVideo) {
         this.displayIncomingCall(uuid, number, callerName, hasVideo, null);
@@ -476,11 +471,6 @@ public class RNCallKeepModule extends ReactContextBaseJavaModule implements Life
 
         conn.onAnswer();
     }
-
-    // @ReactMethod
-    // public void startCall(String uuid, String number, String callerName) {
-    //     this.startCall(uuid, number, callerName, false, null);
-    // }
 
     @ReactMethod
     public void startCall(String uuid, String number, String callerName, boolean hasVideo) {
